eaafb9ff1f243ece8a9374108b5695713105c0b3
gregoiredierendonck <bonjour@gregoiredierendonck.fr>
2015-06-22 13:15:17 +0200

    Edit config.rb to resolve build paths

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ end
 
 require 'slim'
 activate :livereload
-activate :directory_indexes
+# activate :directory_indexes
 
 set :js_dir, 'assets/javascripts'
 set :css_dir, 'assets/stylesheets'
@@ -12,20 +12,16 @@ set :images_dir, 'assets/images'
 
 # Add bower's directory to sprockets asset path
 after_configuration do
-
   @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
   sprockets.append_path File.join "#{root}", @bower_config["directory"]
-
 end
 
 # Build-specific configuration
 configure :build do
-
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-
   activate :relative_assets
+  activate :gzip
   set :relative_links, true
-
 end
