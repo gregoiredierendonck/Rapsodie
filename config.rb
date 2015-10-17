4a0dda7e0eb65e0b258f3452ac7a774ba889951c
gregoiredierendonck <bonjour@gregoiredierendonck.fr>
2015-10-17 18:27:31 +0200

    Add content to presse + Review some bugs

--- config.rb
+++ config.rb
@@ -36,23 +36,6 @@ end
 
 # Build-specific configuration
 configure :build do
-	activate :favicon_maker do |f|
-		f.template_dir  = File.join(root, 'source')
-		f.output_dir    = File.join(root, 'build')
-		f.icons = {
-			'favicon_base.png' => [
-				{ icon: 'chrome-touch-icon-192x192.png' },
-				{ icon: 'apple-touch-icon.png', size: '152x152' },
-				{ icon: 'ms-touch-icon-144x144-precomposed.png', size: '144x144' },
-				{ icon: 'favicon-196x196.png' },
-				{ icon: 'favicon-160x160.png' },
-				{ icon: 'favicon-96x96.png' },
-				{ icon: 'favicon-32x32.png' },
-				{ icon: 'favicon-16x16.png' },
-				{ icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' },
-			]
-		}
-	end
 	activate :minify_css
 	activate :minify_javascript
 	activate :imageoptim
