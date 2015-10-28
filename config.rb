60693784094e52f5abd08ff136570a92e7074044
Grégoire Dierendonck <bonjour@gregoiredierendonck.fr>
2015-10-28 17:49:09 +0100

    Test on config.rb

--- config.rb
+++ config.rb
@@ -44,9 +44,7 @@ configure :build do
 	activate :minify_css
 	activate :minify_javascript
 	activate :imageoptim
-	activate :asset_hash
 	activate :cache_buster
-	set :relative_links, true
 	ignore 'imageoptim.manifest.yml'
 end
 
